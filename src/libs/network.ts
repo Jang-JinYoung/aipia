95de5dd230ebcc89c1b3aa3acb958fa747aba3a6
jyjang <jinyoung_8965@naver.com>
2025-10-23 14:43:53 +0900

    feat: axios 기본 추가

---
+++ src/libs/network.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+// 공통 axios 인스턴스 생성
+const axiosInstance = axios.create({
+    baseURL: "https://hacker-news.firebaseio.com/v0", // API 기본 URL
+    timeout: 5000, // 요청 제한 시간 (ms)
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+// 요청 인터셉터 (예: 토큰 자동 추가)
+axiosInstance.interceptors.request.use(
+    (config) => {
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+// 응답 인터셉터 (에러 핸들링)
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default axiosInstance;
